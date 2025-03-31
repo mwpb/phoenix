2e95dfd19b83921afc33238dc41292cdfbcc409b
Matthew Burke <matthew@rijalburke.com>
2025-03-31 08:25:55 +0100

    Add configuration

---
+++ phoenix.js
@@ -0,0 +1,104 @@
+let layouts = ["FullScreen", "TwoPane"];
+let currentLayout = 1;
+let stickyWindow = undefined;
+
+const tileLeft = (window) => {
+    let rectangle = Screen.main().visibleFrame();
+    rectangle.width = rectangle.width / 2;
+    window.setFrame(rectangle);
+}
+
+const tileRight = (window) => {
+    let rectangle = Screen.main().visibleFrame();
+    rectangle.width = rectangle.width / 2;
+    rectangle.x = rectangle.width;
+    window.setFrame(rectangle);
+}
+
+const setStickyWindow = (window) => {
+    tileRight(window);
+    stickyWindow = window;
+}
+
+const tileNextWindow = (window) => {
+    if (layouts[currentLayout] === "FullScreen") return window.maximise();
+    if (stickyWindow && window.hash() === stickyWindow.hash()) return tileRight(window);
+    return tileLeft(window);
+}
+
+const focusNextWindow = (app, appLauncher) => {
+    let windowCandidates = app.windows({ visible: true });
+    if (windowCandidates.length === 0) return App.launch(appLauncher, { focus: true });
+    if (app.name() !== App.focused().name()) return windowCandidates[0].focus();
+    return windowCandidates[windowCandidates.length - 1].focus();
+}
+
+const launchOrCycle = (appName, appLauncher) => {
+    let app = App.get(appName);
+    focusNextWindow(app, appLauncher);
+    tileNextWindow(Window.focused());
+}
+
+const cycleLayouts = () => {
+    currentLayout = (currentLayout + 1) % layouts.length;
+    modal.text = layouts[currentLayout];
+    modal.show();
+
+    if (layouts[currentLayout] == "TwoPane") {
+        let focusedWindow = Window.focused();
+        if (stickyWindow) {
+            tileRight(stickyWindow);
+            stickyWindow.focus();
+        }
+        tileLeft(focusedWindow);
+        focusedWindow.focus();
+        return;
+    }
+
+    if (layouts[currentLayout] == "FullScreen") {
+        Window.focused().maximise();
+        return;
+    }
+}
+
+const screenFrame = Screen.main().flippedVisibleFrame();
+const modal = Modal.build({duration: 1., text: "no"})
+modal.origin = {
+    x: screenFrame.width / 2 - modal.frame().width / 2,
+    y: screenFrame.height / 2 - modal.frame().height / 2,
+}
+
+const modifiers = ["command", "alt", "control"]
+
+let safariHandler = new Key("s", modifiers, () => {
+    launchOrCycle("Safari", "Safari");
+});
+let calendarHandler = new Key("c", modifiers, () => {
+    launchOrCycle("Calendar", "Calendar");
+});
+let codeHandler = new Key("v", modifiers, () => {
+    launchOrCycle("Code", "Visual Studio Code");
+});
+let remindersHandler = new Key("r", modifiers, () => {
+    launchOrCycle("Reminders", "Reminders");
+});
+let mailHandler = new Key("m", modifiers, () => {
+    launchOrCycle("Mail", "Mail");
+});
+let terminalHandler = new Key("t", modifiers, () => {
+    launchOrCycle("Terminal", "Terminal");
+});
+let finderHandler = new Key("f", modifiers, () => {
+    launchOrCycle("Finder", "Finder");
+});
+let githubHandler = new Key("g", modifiers, () => {
+    launchOrCycle("GitHub Desktop", "GitHub Desktop");
+});
+
+let setStickyHandler = new Key("return", modifiers, () => {
+    setStickyWindow(Window.focused());
+});
+
+let layoutHandler = new Key("space", modifiers, () => {
+    cycleLayouts();
+});
